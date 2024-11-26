2e229fe8edc11739c93dae586717a824bc20fcb3
LindaMRL19 <linda.lopez111@std.uni.edu.ni>
2024-11-25 18:35:10 -0600

    primer cambio confirmado

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace GestionProductosXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lstProducto = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Precio : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(354, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cantidad : ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(135, 64);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(520, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(113, 105);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(200, 20);
+            this.txtPrecio.TabIndex = 4;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(442, 108);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(147, 20);
+            this.txtCantidad.TabIndex = 5;
+            // 
+            // lstProducto
+            // 
+            this.lstProducto.FormattingEnabled = true;
+            this.lstProducto.Location = new System.Drawing.Point(68, 167);
+            this.lstProducto.Name = "lstProducto";
+            this.lstProducto.Size = new System.Drawing.Size(633, 173);
+            this.lstProducto.TabIndex = 6;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(35, 380);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(141, 45);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar Producto";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(231, 380);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(138, 45);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar en XML";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.Location = new System.Drawing.Point(416, 380);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(160, 45);
+            this.btnCargar.TabIndex = 9;
+            this.btnCargar.Text = "Cargar desde XML";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(617, 380);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(84, 45);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lstProducto);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.ListBox lstProducto;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace GestionProductosXML
+{
+    public partial class Form1 : Form
+    {
+        private List<Producto> productos = new List<Producto>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // Validar que los campos no estén vacíos
+            if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtPrecio.Text) || string.IsNullOrEmpty(txtCantidad.Text))
+            {
+                MessageBox.Show("Todos los campos son obligatorios.");
+                return;
+            }
+
+            // Crear un nuevo producto con los valores ingresados
+            Producto nuevoProducto = new Producto
+            {
+                Nombre = txtNombre.Text,
+                Precio = Convert.ToDecimal(txtPrecio.Text),
+                Cantidad = Convert.ToInt32(txtCantidad.Text)
+            };
+
+            // Agregar el producto a la lista
+            productos.Add(nuevoProducto);
+
+            // Mostrar el producto en el ListBox
+            lstProducto.Items.Add($"{nuevoProducto.Nombre} - ${nuevoProducto.Precio} - {nuevoProducto.Cantidad} unidades");
+
+            // Limpiar los campos para el siguiente ingreso
+            txtNombre.Clear();
+            txtPrecio.Clear();
+            txtCantidad.Clear();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Ruta donde se guardará el archivo XML
+                string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\productos.xml";
+
+                // Serializar la lista de productos a XML
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Producto>));
+                using (StreamWriter writer = new StreamWriter(ruta))
+                {
+                    serializer.Serialize(writer, productos);
+                }
+
+                MessageBox.Show("Productos guardados en XML correctamente.", "Éxito");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar en XML: " + ex.Message, "Error");
+            }
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Ruta del archivo XML
+                string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\productos.xml";
+
+                // Verificar si el archivo existe
+                if (!File.Exists(ruta))
+                {
+                    MessageBox.Show("No se encontró el archivo XML.", "Advertencia");
+                    return;
+                }
+
+                // Deserializar el archivo XML a una lista de productos
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Producto>));
+                using (StreamReader reader = new StreamReader(ruta))
+                {
+                    productos = (List<Producto>)serializer.Deserialize(reader);
+                }
+
+                // Actualizar el ListBox
+                lstProducto.Items.Clear();
+                foreach (var producto in productos)
+                {
+                    lstProducto.Items.Add($"{producto.Nombre} - ${producto.Precio} - {producto.Cantidad} unidades");
+                }
+
+                MessageBox.Show("Productos cargados desde XML correctamente.", "Éxito");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar desde XML: " + ex.Message, "Error");
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            var resultado = MessageBox.Show("¿Seguro que deseas salir?", "Confirmar salida", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (resultado == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
